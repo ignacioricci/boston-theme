8b051aeaa2ae792504c19ca7607909f22eecfbf5
Ignacio Ricci <ignacio.ricci@gmail.com>
2013-05-18 11:54:52 -0300

    first commit

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "styles/css"
+sass_dir = "styles/scss"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :nested
+environment = :development
+relative_assets = true
+output_style = :nested # by Compass.app 
