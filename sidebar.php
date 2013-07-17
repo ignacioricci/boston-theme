f25374d2ef78c22a19c6119ddce838fd4a08356a
Ignacio Ricci <nacho@vulsai.com>
2013-07-17 13:57:28 -0300

    logo

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,21 @@
 <aside id="sidebar">
 	<div id="sidebarHolder">
 		<div id="logo">
-			<img src="images/avatar.jpeg">
+			<img src="<?php bloginfo('template_directory'); ?>/images/avatar.jpeg">
 		</div>
 		<div id="blogInfo">
-			<h1>My Blog</h1>
-			<p>A blog about nothing. Yeah, nothing</p>
+			<h1><a href="<?php bloginfo('url'); ?>/"><?php echo bloginfo('title'); ?></a></h1>
+			<p><?php echo bloginfo('description'); ?></p>
 			<nav id="sections">
 				<ul>
 					<?php wp_list_pages('title_li='); ?>
 				</ul>
 			</nav>
 		</div>
+		<!--
 		<div id="search">
 			<input type="text" placeholder="Search">
 		</div>
+	-->
 	</div>
 </aside>
