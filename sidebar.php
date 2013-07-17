85e39207dcf52e99ace7a754cc876d73ccfc8634
Ignacio Ricci <nacho@vulsai.com>
2013-07-17 13:45:45 -0300

    more styles

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,11 @@
 		<div id="blogInfo">
 			<h1>My Blog</h1>
 			<p>A blog about nothing. Yeah, nothing</p>
-		</div>
-		<div id="sections">
-			<ul>
-				<?php wp_list_pages('title_li='); ?>
-			</ul>
+			<nav id="sections">
+				<ul>
+					<?php wp_list_pages('title_li='); ?>
+				</ul>
+			</nav>
 		</div>
 		<div id="search">
 			<input type="text" placeholder="Search">
