69e15789366ffd6e57d4248e5c05789b9a97aa5d
Ignacio Ricci <ignacio.ricci@gmail.com>
2013-08-25 13:19:47 -0300

    Fix IE bullshit

--- header.php
+++ header.php
@@ -36,6 +36,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/styles/css/boston.css">
 	<!--[if lte IE 9]>
 	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/html5shiv.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/styles/css/ie.css">
 	<![endif]-->
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/images/iphone.png" />
